1f5f3b737ac64d3aa86630670ae166fa54806af8
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2019-04-08 16:58:24 -0400

    fitting

---
+++ FitInterarrivalTimes.py
@@ -0,0 +1,40 @@
+from SimPy import InOutFunctions as InOutSupport
+from SimPy import FigureSupport as Fig
+from SimPy import StatisticalClasses as Stat
+from SimPy import FittingProbDist_ML as FitML
+
+# read interarrival times
+cols = InOutSupport.read_csv_cols(file_name='dataInterarrivalTimes.csv',
+                                  n_cols=1,
+                                  if_ignore_first_row=True,
+                                  if_convert_float=True)
+
+# make a histogram
+Fig.graph_histogram(data=cols[0],
+                    title='Interarrival Times (Minutes)',
+                    bin_width=0.5
+                    )
+
+# mean and standard deviation
+stat = Stat.SummaryStat(name='Interarrival times',
+                        data=cols[0])
+print('Mean = ', stat.get_mean())
+print('StDev = ', stat.get_stdev())
+
+# fit an exponential distribution
+fit_results = FitML.fit_exp(data=cols[0],
+                            x_label='Interarrival Times',
+                            bin_width=0.5)
+print('Fitting an exponential distribution:', fit_results)
+
+# fit a gamma distribution
+fit_results = FitML.fit_gamma(data=cols[0],
+                              x_label='Interarrival Times',
+                              bin_width=0.5)
+print('Fitting a gamm distribution:', fit_results)
+
+# fit a log-normal distribution
+fit_results = FitML.fit_lognorm(data=cols[0],
+                                x_label='Interarrival Times',
+                                bin_width=0.5)
+print('Fitting a log-normal distribution:', fit_results)
---
+++ FitNumOfDrinks.py
@@ -0,0 +1,40 @@
+from SimPy import InOutFunctions as InOutSupport
+from SimPy import FigureSupport as Fig
+from SimPy import StatisticalClasses as Stat
+from SimPy import FittingProbDist_ML as FitML
+
+# read weekly number of drinks
+cols = InOutSupport.read_csv_cols(file_name='dataNumOfDrinks.csv',
+                                  n_cols=1,
+                                  if_ignore_first_row=True,
+                                  if_convert_float=True)
+
+# make a histogram
+Fig.graph_histogram(data=cols[0],
+                    title='Weekly Number of Drinks',
+                    bin_width=1
+                    )
+
+# mean and standard deviation
+stat = Stat.SummaryStat(name='Weekly number of drinks',
+                        data=cols[0])
+print('Mean = ', stat.get_mean())
+print('StDev = ', stat.get_stdev())
+
+# fit a Poisson distribution
+fit_results = FitML.fit_poisson(data=cols[0],
+                                x_label='Weekly number of drinks',
+                                bin_width=1)
+print('Fitting a Poisson distribution:', fit_results)
+
+# fit a gamma-Poisson distribution
+fit_results = FitML.fit_gamma_poisson(data=cols[0],
+                                      x_label='Weekly number of drinks',
+                                      bin_width=1)
+print('Fitting a gamma-Poisson distribution:', fit_results)
+
+# fit a beta-binomial distribution
+fit_results = FitML.fit_beta_binomial(data=cols[0],
+                                      x_label='Weekly number of drinks',
+                                      bin_width=1)
+print('Fitting a beta-binomial distribution:', fit_results)
