59c1b7220ad208ce9c7a93b54be331a18913d27d
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2022-12-19 22:52:34 -0500

    2023

--- FitInterarrivalTimes.py
+++ FitInterarrivalTimes.py
@@ -1,23 +1,23 @@
-import SimPy.InOutFunctions as IO
-import SimPy.Plots.Histogram as Hist
-import SimPy.Plots.ProbDist as Plot
-import SimPy.RandomVariateGenerators as RVGs
-import SimPy.Statistics as Stat
+import deampy.in_out_functions as io
+import deampy.plots.histogram as hist
+import deampy.plots.prob_dists as plot_dist
+import deampy.random_variats as RVGs
+import deampy.statistics as stats
 
 # read interarrival times
-cols = IO.read_csv_cols(file_name='dataInterarrivalTimes.csv',
+cols = io.read_csv_cols(file_name='dataInterarrivalTimes.csv',
                         n_cols=1,
                         if_ignore_first_row=True,
                         if_convert_float=True)
 
 # make a histogram
-Hist.plot_histogram(data=cols[0],
+hist.plot_histogram(data=cols[0],
                     title='Interarrival Times (Minutes)',
                     bin_width=0.5)
 
 # mean and standard deviation
-stat = Stat.SummaryStat(name='Interarrival times',
-                        data=cols[0])
+stat = stats.SummaryStat(name='Interarrival times',
+                         data=cols[0])
 print('Mean = ', stat.get_mean())
 print('StDev = ', stat.get_stdev())
 
@@ -26,7 +26,7 @@ fit_results = RVGs.Exponential.fit_ml(data=cols[0])
 print('Fitting an exponential distribution:', fit_results)
 
 # plot the fitted exponential distribution
-Plot.plot_exponential_fit(
+plot_dist.plot_exponential_fit(
     data=cols[0], fit_results=fit_results, x_label='Interarrival Times', x_range=(0, 20), bin_width=0.5)
 
 
@@ -35,7 +35,7 @@ fit_results = RVGs.Gamma.fit_ml(data=cols[0])
 print('Fitting a gamma distribution:', fit_results)
 
 # plot the fitted gamma distribution
-Plot.plot_gamma_fit(
+plot_dist.plot_gamma_fit(
     data=cols[0], fit_results=fit_results, x_label='Interarrival Times', x_range=(0, 20), bin_width=0.5)
 
 # fit a log-normal distribution
@@ -43,5 +43,5 @@ fit_results = RVGs.LogNormal.fit_ml(data=cols[0])
 print('Fitting a log-normal distribution:', fit_results)
 
 # plot the fitted log-normal distribution
-Plot.plot_lognormal_fit(
+plot_dist.plot_lognormal_fit(
     data=cols[0], fit_results=fit_results, x_label='Interarrival Times', x_range=(0, 20), bin_width=0.5)
--- FitNumOfDrinks.py
+++ FitNumOfDrinks.py
@@ -1,23 +1,23 @@
-import SimPy.InOutFunctions as IO
-import SimPy.Plots.Histogram as Hist
-import SimPy.Plots.ProbDist as Plot
-import SimPy.RandomVariateGenerators as RVGs
-import SimPy.Statistics as Stat
+import deampy.in_out_functions as io
+import deampy.plots.histogram as hist
+import deampy.plots.prob_dists as plot_dist
+import deampy.random_variats as RVGs
+import deampy.statistics as stats
 
 # read weekly number of drinks
-cols = IO.read_csv_cols(file_name='dataNumOfDrinks.csv',
+cols = io.read_csv_cols(file_name='dataNumOfDrinks.csv',
                         n_cols=1,
                         if_ignore_first_row=True,
                         if_convert_float=True)
 
 # make a histogram
-Hist.plot_histogram(data=cols[0],
+hist.plot_histogram(data=cols[0],
                     title='Weekly Number of Drinks',
                     bin_width=1)
 
 # mean and standard deviation
-stat = Stat.SummaryStat(name='Weekly number of drinks',
-                        data=cols[0])
+stat = stats.SummaryStat(name='Weekly number of drinks',
+                         data=cols[0])
 print('Mean = ', stat.get_mean())
 print('StDev = ', stat.get_stdev())
 
@@ -26,7 +26,7 @@ fit_results = RVGs.Poisson.fit_ml(data=cols[0])
 print('Fitting a Poisson distribution:', fit_results)
 
 # plot the fitted Poisson distribution
-Plot.plot_poisson_fit(
+plot_dist.plot_poisson_fit(
     data=cols[0], fit_results=fit_results, x_label='Weekly number of drinks', x_range=(0, 40), bin_width=1)
 
 # fit a gamma-Poisson distribution
@@ -34,7 +34,7 @@ fit_results = RVGs.GammaPoisson.fit_ml(data=cols[0])
 print('Fitting a gamma-Poisson distribution:', fit_results)
 
 # plot the fitted gamma-Poisson distribution
-Plot.plot_gamma_poisson_fit(
+plot_dist.plot_gamma_poisson_fit(
     data=cols[0], fit_results=fit_results, x_label='Weekly number of drinks', x_range=(0, 40), bin_width=1)
 
 # fit a beta-binomial distribution
@@ -42,5 +42,5 @@ fit_results = RVGs.BetaBinomial.fit_ml(data=cols[0])
 print('Fitting a beta-binomial distribution:', fit_results)
 
 # plot the fitted beta-binomial distribution
-Plot.plot_beta_binomial_fit(
+plot_dist.plot_beta_binomial_fit(
     data=cols[0], fit_results=fit_results, x_label='Weekly number of drinks', x_range=(0, 40), bin_width=1)
