17db1efc59581ddb6717a4feac9e57ea46618d23
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2023-01-19 18:46:45 -0500

    2023

--- MethodOfMoment.py
+++ MethodOfMoment.py
@@ -1,16 +1,14 @@
-import SimPy.RandomVariateGenerators as RVGs
+import deampy.random_variats as rvgs
 
 # fitting an exponential distribution
-exp_params = RVGs.Exponential.fit_mm(mean=10)
+exp_params = rvgs.Exponential.fit_mm(mean=10)
 print('Parameters of exponential distribution:', exp_params)
 
 # fitting a beta distribution
-beta_params = RVGs.Beta.fit_mm(mean=20,
-                               st_dev=5,
-                               minimum=0,
-                               maximum=50)
+beta_params = rvgs.Beta.fit_mm(
+    mean=20, st_dev=5, minimum=0,maximum=50)
 print('Parameters of beta distribution:', beta_params)
 
 # fitting a Poisson distribution
-poisson_params = RVGs.Poisson.fit_mm(mean=20)
+poisson_params = rvgs.Poisson.fit_mm(mean=20)
 print('Parameters of Poisson distribution', poisson_params)
