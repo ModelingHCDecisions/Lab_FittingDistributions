2c6fa50f9b05a8ec61d0e844dea99700fbf6af6f
rezayas <reza.yaesoubi@yale.edu>
2024-04-12 15:07:34 -0400

    2024

--- GenerateRandomRealizations.py
+++ GenerateRandomRealizations.py
@@ -1,22 +1,21 @@
-import numpy as np
-
 import deampy.random_variates as rvgs
+import numpy as np
 
 # a random number generator
 rng = np.random.RandomState(seed=0)
 
 # gamma
-gamma = rvgs.Gamma(a=0.9835458832943496,
-                   loc=0,
-                   scale=1.9199027077975956)
+gamma = rvgs.Gamma(shape=0.9835458832943496,
+                   scale=1.9199027077975956,
+                   loc=0)
 # generate 10 realizations from gamma
 print('Realizations from gamma:')
 for i in range(10):
     print(gamma.sample(rng))
 
 # gamma-Poisson
-gammaPoisson = rvgs.GammaPoisson(a=0.3693765965041004,
-                                 gamma_scale=9.154827879319111,
+gammaPoisson = rvgs.GammaPoisson(shape=0.3693765965041004,
+                                 scale=9.154827879319111,
                                  loc=0)
 # generate 10 realizations from gamma-Poisson
 print('Realizations from gamma-Poisson:')
